e97728b35741b34bcbd8a13911e5e55c649c7bb5
Rahul <rg4005450@gmail.com>
2023-06-01 17:12:25 +0530

    Now the chat gpt is created but it is not resposive working on it's responsiveness

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -2,24 +2,40 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import gptico from "../Assets/gptico.png";
+import "../Chat.css";
+import chat from "../Assets/chat.gif";
 
 function Chat(props) {
+  const { question, answer, chatLoader } = props;
+
   return (
     <>
-      <main className="w-[100%] h-[70vh] flex flex-col justify-start">
+      <main
+        className="w-[100%] h-[70vh] flex flex-col justify-start"
+        id="chatElement"
+      >
         <div className="w-full hover:bg-gray-700 hover:trasition duration-75 ease-in-out">
           <div className="flex mb-5 mt-3">
             <FontAwesomeIcon
               icon={faUser}
-              className="mx-2 h-[26px] py-[12px] px-[14px] rounded-full border border-white"
+              className="mx-10 h-[26px] py-[12px] px-[14px] rounded-full border border-white"
             />
-            <p className="text-2xl relative top-[5px]">: I am question</p>
+            <p className="text-2xl relative top-[5px]"> {question}</p>
           </div>
         </div>
-        <div className="w-full hover:bg-gray-700 hover:trasition duration-75 ease-in-out">
-          <div className="flex mt-3">
-            <img src={gptico} className="h-[4.75rem] py-[12px] px-[14px]" />
-            <p className="text-2xl relative top-[18px]">: I am answer</p>
+        <div className="w-full hover:trasition duration-75 ease-in-out">
+          <div className="flex mt-3 hover:bg-gray-700  max-h-full">
+            <img
+              src={gptico}
+              className="h-[4.75rem] py-[12px] px-[14px] mx-7"
+            />
+            <p className="text-2xl relative top-[6px] mb-10">
+              {chatLoader ? (
+                <img src={chat} className="h-[182px] w-[11rem] my-[-69px]" />
+              ) : (
+                answer
+              )}
+            </p>
           </div>
         </div>
       </main>
